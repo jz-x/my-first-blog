244e3cc6aed2a43525e0fe34092ede21125e8841
jerry <jieruiz2016@gmail.com>
2017-08-03 17:26:03 +0800

    Modified templates to display posts from DB

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 # Create your models here.
-
+# Model 1: Post
 class Post(models.Model):
     
     author = models.ForeignKey('auth.User')
@@ -16,7 +16,7 @@ class Post(models.Model):
         
     def __str__(self):
         return self.title
-    
+ 
 
 
 '''from django.utils import timezone
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Post
+from django.utils import timezone
 
 def post_list (request):
     
-    return render (request, 'blog/post_list.html', {})
+    # treat variable posts as the name of the QuerySet
+    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    
+    # {} is the place to add some things for the template to use
+    return render(request, 'blog/post_list.html', {'posts':posts})
