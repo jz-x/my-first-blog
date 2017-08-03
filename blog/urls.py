e21974f63a26b53b02c19eda0b9a0110a4b9cf46
jerry <jieruiz2016@gmail.com>
2017-08-03 13:49:31 +0800

    Change HTML for the site

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  3 10:46:37 2017
+
+@author: Jerry
+"""
+
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [ url(r'^$', views.post_list, name='post_list')]
